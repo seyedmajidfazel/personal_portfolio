0aac01dcba0d5f15e2ed1c51d57e300f9054191a
seyedmajidfazel <seyedmajidfazel@gmail.com>
2020-04-10 23:44:58 +0430

    first try

---
+++ blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Blog(models.Model):
+    title=models.CharField(max_length=200)
+    description=models.TextField(max_length=250)
+    date=models.DateField()
+    def __str__(self):
+        return self.title
